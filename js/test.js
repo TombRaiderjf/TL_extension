503b27a1bdc106bac0324519c4da35234d014160
TombRaiderjf <jiangfanloveen@outlook.com>
2019-07-04 21:36:05 +0800

    改回原来的验证码api

--- js/test.js
+++ js/test.js
@@ -43,103 +43,110 @@ function getImgBase64(path, callback) {
  * @param {function} error_callback 
  */
 function recognizeCaptcha(image, succ_callback, error_callback) {
-    // let api_url = 'http://api.3023data.com/ocr/captcha';
-    // let method = 'POST';
-    // let appcode = '563ccc798055f2b707b3817425e38afe';
-
-    // $.ajax({
-    //     type: method,
-    //     url: api_url,
-    //     beforeSend: function (request) {
-    //         request.setRequestHeader("key", appcode);
-    //     },
-    //     data: {
-    //         image: image,
-    //         length: 4,
-    //         type: 1001
-    //     },
-    //     success: succ_callback,
-    //     error: error_callback
-    // });
+    let api_url = 'http://api.3023data.com/ocr/captcha';
+    let method = 'POST';
+    let appcode = '563ccc798055f2b707b3817425e38afe';
 
     $.ajax({
-        type: 'post',
-        url: 'http://route.showapi.com/184-5',
-        dataType: 'json',
+        type: method,
+        url: api_url,
+        beforeSend: function (request) {
+            request.setRequestHeader("key", appcode);
+        },
         data: {
-            "showapi_timestamp": '',
-            "showapi_appid": '99277', //这里需要改成自己的appid
-            "showapi_sign": '107bcec39adc4ef9ae02eb16ab789565',  //这里需要改成自己的应用的密钥secret
-            "img_base64": image,
-            "typeId":"34",
-            "convert_to_jpg":"0",
-            "needMorePrecise":"0"
+            image: image,
+            length: 4,
+            type: 1001
         },
         success: succ_callback,
         error: error_callback
     });
+
+    // $.ajax({
+    //     type: 'post',
+    //     url: 'http://route.showapi.com/184-5',
+    //     dataType: 'json',
+    //     data: {
+    //         "showapi_timestamp": '',
+    //         "showapi_appid": '99277', //这里需要改成自己的appid
+    //         "showapi_sign": '107bcec39adc4ef9ae02eb16ab789565',  //这里需要改成自己的应用的密钥secret
+    //         "img_base64": image,
+    //         "typeId":"34",
+    //         "convert_to_jpg":"0",
+    //         "needMorePrecise":"0"
+    //     },
+    //     success: succ_callback,
+    //     error: error_callback
+    // });
  
 }
 
 function run() {
-    var date1=new Date();  //开始时间
-    clearInterval(itv_run);
-    let imgLink = image_url + "&t=" + (new Date).getTime();
-    getImgBase64(imgLink, function (dataUrl) {           
-        // console.log(dataUrl);
-        recognizeCaptcha(dataUrl,
-            function(data) {
-                console.log(data.showapi_res_body.Result);
-                $.ajax({
-                    url: "http://tl.cyg.changyou.com/transaction/buy",
-                    type: "post",
-                    async: !1,
-                    data: {
-                        goods_serial_num: id,
-                        captcha_code: data.showapi_res_body.Result
-                    },
-                    success: function(t) {
-                        console.log(t);
-                        if (t != "captcha_error"){
-                            var date2=new Date();    //结束时间
-                            var date3=date2.getTime()-date1.getTime()  //时间差的毫秒数
-                            console.log("time=");
-                            console.log(date3);
+    // clearInterval(itv_run);
+    let time = $("span.less-than-day").text();
+    if (time == "公示完成"){    
+    // if (true){  //交易区下单
+        
+        var date1=new Date();  //开始时间
+        console.log(date1);
+        let imgLink = image_url + "&t=" + (new Date).getTime();
+        getImgBase64(imgLink, function (dataUrl) {   
+            if (dataUrl == ''){
+                console.log("获取验证码失败");
+                return;
+            }        
+            // console.log(dataUrl);
+            var date_img = new Date();
+            console.log(date_img.getTime()-date1.getTime());
+            recognizeCaptcha(dataUrl,
+                function(data) {
+                    console.log(data.data.captcha);
+                    var date_post = new Date();
+                    console.log(date_post.getTime() - date_img.getTime());
+                    $.ajax({
+                        url: "http://tl.cyg.changyou.com/transaction/buy",
+                        type: "post",
+                        async: !1,
+                        data: {
+                            goods_serial_num: id,
+                            captcha_code: data.data.captcha
+                        },
+                        success: function(t) {
+                            console.log(t);
+                            if (t != "captcha_error"){
+                                clearInterval(itv_run);
+                                // var date2=new Date();    //结束时间
+                                // var date3=date2.getTime()-t1.getTime()  //时间差的毫秒数
+                                // console.log(date2);
+                                // console.log("time=");
+                                // console.log(date3);
+                            }
                         }
-                    }
+                    });
+                },
+                function (data) {
+                    alert(`错误:\n${data}`);
                 });
-            },
-            function (data) {
-                alert(`错误:\n${data}`);
-            });
-    });
-    
-
+        });
+    }
     return true;
 }
 
-var date1=new Date();  //开始时间
-
-
-let start = false;
-let executing = false;
-
-
-
-let check_time = setInterval(()=>{
-    let time = $("span.less-than-day").text();
-    if (time == "公示完成"){
-        start = true;   
-        clearInterval(check_time);        
-    }
-}, 100);
-
-
+var t1 = new Date()
 
 let itv_run = setInterval(() => {
-    if (start && !executing) {
-        run();
-        executing = true;       
-    }
-}, 100);
+    // clearInterval(itv_run);
+    // for(var y = 0;y<5;y++){
+    //     run();
+    //     var sum = 0;
+    //     for(var x=0;i<100000;x++)
+    //     {
+    //         sum += x;
+    //     }
+    // }
+    run();
+}, 1000);
+
+
+
 
